6e366d6cb75147c4072093df01e28586cb5d8a51
rahulgupta <rahul1502.iimt@gmail.com>
2024-02-10 11:47:12 +0530

    Binary search Tree

--- Data structure /BinarySearchTree.java
+++ Data structure /BinarySearchTree.java
@@ -1,3 +1,32 @@
 public class BinarySearchTree {
+    private class TreeNode{
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+    public TreeNode root;
+
+    public TreeNode insert(TreeNode root,int value){
+        if(root == null){
+            root =  new TreeNode(value);
+            return root;
+        }
+        if(value<root.data){
+            root.left = insert(root.left,value);
+        }
+        else{
+            root.right = insert(root.right,value);
+        }
+        return root;
+    }
+    public  TreeNode search(TreeNode root,int key){
+        if(root == null || root.data == key) return root;
+        if(key< root.data) return search(root.left,key);
+        else return search(root.right,key);
+    }
     
 }
